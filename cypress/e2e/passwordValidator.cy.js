1e065e5210ad65530426e1b11c5afe190297a9cd
rsa-key-20201102 <mostowyjb@gmail.com>
2023-05-24 15:19:55 +0200

    check nb char.

--- cypress/e2e/passwordValidator.cy.js
+++ cypress/e2e/passwordValidator.cy.js
@@ -3,7 +3,14 @@ describe('Password Validation', () => {
       cy.visit('/');
       cy.get('input').type('abc123$');
       cy.get('button').click();
-      cy.contains('Le mot de passe doit comporter au moins 8 caractères');
+      cy.get('#errorMessages').should('contain', 'Le mot de passe doit comporter au moins 8 caractères');
+    });
+
+    it('Vérifie que le mot de passe doit contenir au moins 2 chiffres', () => {
+      cy.visit('/');
+      cy.get('input').type('password');
+      cy.get('button').click();
+      cy.get('#errorMessages').should('contain', 'Le mot de passe doit contenir au moins 2 chiffres');
     });
   
     // Ajoutez les autres tests pour les conditions restantes
--- src/passwordValidator.js
+++ src/passwordValidator.js
@@ -8,7 +8,13 @@ export function validatePassword(password) {
       validationResult.valid = false;
       validationResult.errors.push('Le mot de passe doit comporter au moins 8 caractères');
     }
-  
+    
+    // Vérification du nombre de chiffres
+    const digitCount = password.match(/[0-9]/g)?.length || 0;
+    if (digitCount < 2) {
+      validationResult.valid = false;
+      validationResult.errors.push("Le mot de passe doit contenir au moins 2 chiffres");
+    } 
     // Ajoutez les autres conditions de validation et les messages d'erreur correspondants
   
     return validationResult;
